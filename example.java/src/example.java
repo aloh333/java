f0f6b1369b27233722dda87da79214edd1b2d825
aloh333 <al3190589@gmail.com>
2022-09-20 00:49:13 +0900

    first commit

---
+++ example.java/src/example.java
@@ -0,0 +1,13 @@
+public class example {
+    public static void main (String[] args) {
+        int a[] = new int[8];
+        int i = 0, n = 10;
+        while (n > 0) {
+            a[i++] = n % 2;
+            n /= 2;
+        }
+        for (i = 7; i >= 0; i--) {
+            System.out.printf("%d", a[i]);
+        }
+    }
+}
